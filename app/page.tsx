a6fa13f21754fa0aa05e7ee73732e8687a3610f3
Prajapati vishal <107975264+vishal007p@users.noreply.github.com>
2025-08-25 18:03:49 +0530

    asdas

--- app/page.tsx
+++ app/page.tsx
@@ -45,8 +45,9 @@ export default function SignInPage() {
         toast.error(error || "Login failed ❌");
         return;
       }
-      Cookies.set("token", data.token, { expires: 1, path: "/" });
       localStorage.setItem("token", data.token)
+
+      Cookies.set("token", data.token, { expires: 1, path: "/" });
       toast.success("Login successful 🎉");
       router.push("/dashboard");
     } catch (err) {
